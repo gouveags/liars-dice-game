63c2d6438729bbc2e9a128028d4e23b9d39a05ca
Gabriel Gouvea <gabriel.silva@ip.tv>
2024-11-15 02:24:37 -0300

    create Dice class

---
+++ dice.js
@@ -0,0 +1,27 @@
+const assert = require('node:assert');
+const { getRandomInt } = require('./utils');
+
+class Dice {
+    #faces = [];
+    #lastDrawnFace = null;
+
+    constructor (numberOfFaces){
+        assert(numberOfFaces > 0, 'Error: Please, instantiate the Dice class with a number of Dice faces greater than zero');
+        for (let i = 0; i < numberOfFaces; i++) {
+            this.#faces[i] = Number(i + 1);
+        }
+        this.#lastDrawnFace = null;
+    }
+
+    shuffle() {
+        assert(!!this.#faces.length, 'Error: Cannot shuffle a Dice without faces');
+        return this.#lastDrawnFace = this.#faces[getRandomInt(0, this.#faces.length)];
+    }
+
+    getLastDrawnFace(){
+        assert(!!this.#lastDrawnFace, 'FatalError: No lastDrawnFace attribute of Dice');
+        return this.#lastDrawnFace;
+    }
+}
+
+module.exports = Dice;
